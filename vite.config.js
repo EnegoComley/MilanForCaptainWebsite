b8fa0866e45e556d0b75db015705a6dd4095aa06
Enego-Tellmi <enegocomley@gmail.com>
2026-03-10 23:36:15 +0000

    Changed the URL

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     port: 3000,
     strictPort: true,
-    allowedHosts: ["milanforcaptain.enego.co.uk"],
+    allowedHosts: ["milanforcaptain.uk"],
     // Prevent stale assets during development.
     headers: {
       "Cache-Control": "no-store"
